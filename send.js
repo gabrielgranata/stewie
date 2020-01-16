0c4d8a4df0f4f0e8cf4bbb395f6f51b6bad61bd7
Gabriel Granata <ggran067@uottawa.ca>
2020-01-15 22:06:42 -0500

    adds exchange to complete broker model

--- send.js
+++ send.js
@@ -1,25 +1,25 @@
 let amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(error0, connection) {
-
-    if (error0) {
-        throw error0;
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
     }
+    let exchange = 'logs';
+    let msg = process.argv.slice(2).join(' ') || "Hello World!";
 
-    connection.createChannel(function(error1, channel) {
-        if (error1) {
-            throw error1;
-        }
-        let queue = 'hello';
-        let msg = 'Hello world';
-
-        channel.assertQueue(queue, {
-            durable: false
-        });
-
-        channel.sendToQueue(queue, Buffer.from(msg));
-        console.log(" [x] Sent %s", msg);
+    channel.assertExchange(exchange, 'fanout', {
+        durable: false
     });
-
-
+    
+    channel.publish(exchange, '', Buffer.from(msg));
+    console.log(" [x] Sent '%s'", msg);
+  });
+  setTimeout(function() {
+    connection.close();
+    process.exit(0)
+  }, 500);
 });
